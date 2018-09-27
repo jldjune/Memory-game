45c85c1a5002699bde9aae85b1f61fa04506fa64
jldjune <jose.diaz23@gmail.com>
2018-09-27 15:04:09 -0400

    Scoring and timer in footer test functions

--- js/memory-game.js
+++ js/memory-game.js
@@ -15,20 +15,41 @@ class MemoryGame{
 		// grab message box
 		this.msgBox = document.getElementById('msg-box');
 		
-		// grab the 
+		// grab the footer that holds score and timer
 		this.footer = document.querySelector('footer');
+		
+		// grab the score box span 
+		this.attemptBox = document.getElementById('attempt-box');
+		this.matchBox = document.getElementById('match-box');
+		this.averageBox = document.getElementById('average-box');
+		this.scoreBox = document.getElementById('score-box');
 		
 		// an array to keep track of choices for comparison
 		this.choicesArr = []
+		
+		// grab timer span
+		this.timer = document.getElementById('timer');
+		
 	} // end constructor
 	
 	// method runs when 'play' button is clicked
-	initGame() {	
+	initGame() {
+		// scoring vars (attempt, match, average, score, scale)
+		this.attempts = 0;
+		this.matches = 0;
+		this.average = 0;
+		this.score = 0;
+		this.scale = 0; 
+		
 		// randomize initial array
 		this.arr.sort((a, b) => 0.5 - Math.random());
 		
 		// get array size from select menu
 		let gameSize = Number(document.getElementById('difficulty').value);
+		
+		// score multiplier based on game difficulty
+		this.scale = gameSize ** 2;
+		
 		
 		// make a new array with a slice of the needed size
 		let gameArr = this.arr.slice(0, gameSize);
@@ -74,51 +95,68 @@ class MemoryGame{
 			}
 		}, 1000)
 		this.hidePic();
+		
 	}// end initGame
 	
 	// method runs when card image is clicked
 	showPic(){
-		
+		// changes src of image clicked
 		event.target.src = 'images/final/200x200/' + event.target.name + '.jpg';
 		
-		// array to hold choices
+		// array to hold images clicked
 		this.choicesArr.push(event.target);
 		
-		// compare when array has 2 items 
+		// start comparison when array has 2 items 
 		if(this.choicesArr.length == 2){
-			alert(this.choices[0].src)
-			// compare names
+			
+			// counts everytime choice array is evaluated
+			this.attempts++;
+			
+			// compare names of both elements in the array
 			if(this.choicesArr[0].name == this.choicesArr[1].name){
-				// names match, check ID's
+				
+				// if names match, check if ID's match
 				if(this.choicesArr[0].id == this.choicesArr[1].id){
-				   // ID's match, must be same image
-					// hide the image
-					this.choicesArr[0].src = 'images/blank.png';
-					this.msgBox.innerHTML = 'You clicked the SAME pic twice!';
+					
+				   // if ID's match, same image was clicked twice
+					this.msgBox.innerHTML = 'Oops! You Clicked the <br>Same Pic Twice!';
+					
+					// hide the image 
+					setTimeout(() => {
+						this.choicesArr[0].src = 'images/blank.png';
+					}, 500)
 					
 				} else {// ID's don't match, it's a legit match
 			   		
 					this.msgBox.innerHTML = "Congrats! That's a Match!";
 					
-				}// end 
+					
+					// counts when player finds a match
+					this.matches++;
+					
+				}// end if ID comparison
 			} else { // names don't match
 				
-				//hide mismatched images
-				setTimeout(function(){
-					this.choicesArr[0].src = 'images/blank.png';
-					this.msgBox.innerHTML = 'Sorry! Try Again!';
-				}, 500)
+				// output message
+				this.msgBox.innerHTML = 'Oops! Try Again!';
 				
-				setTimeout(function(){
+				//hide mismatched images at set delay
+				setTimeout(() => {
+					this.choicesArr[0].src = 'images/blank.png';
+					
+				}, 750)
+				
+				setTimeout(() => {
 					this.choicesArr[1].src = 'images/blank.png';
-				}, 500)		
+				}, 750)		
 				
 			} // end if name comparison
 			
-			// reset array
-//			setTimeout(function(){
-//				this.choicesArr = []
-//			}, 1500)
+			 // reset comparison array
+			setTimeout(() => {
+				this.choicesArr = []
+			}, 1000)
+			
 			
 		} // end if array length
 
@@ -137,13 +175,58 @@ class MemoryGame{
 			/*
 				OR target the children of app div
 				
+				// also hides images without first 'grabbing' them off the DOM
 				for(let i=0; i < gameArr.length; i++){
 				this.app.children[i].src = 'images/blank.png';
 				}
 				
 			*/
+			this.startTimer();
 		}, 6500)
 	}
 	
+	startTimer(){
+		let seconds = 0
+		let totSec =0 
+		let minutes = 0
+		setInterval(() => {
+			// update the countdown span tag number
+			totSec++
+			seconds++
+			
+			if(seconds == 60){
+				seconds = 0
+				minutes++;	
+			}
+			
+			let s = seconds;
+			let m = minutes
+			if(seconds < 10 ){
+				s = '0' + seconds;
+			} 
+			
+			if(minutes < 10){
+				m = '0' + minutes;
+			}
+			
+			this.timer.innerHTML = m + ':' + s;
+			
+			
+		}, 1000)
+		
+		// score
+		this.score = Math.ceil((this.average * this.scale * this.matches * 1000)/ totSec);
+
+		// calculate the average and limit to 3 decimal places
+		this.average = (this.matches / this.attempts).toFixed(3)
+
+		// output score variables to score-box 
+		this.attemptBox.innerHTML = this.attempts;
+		this.matchBox.innerHTML = this.matches;
+		this.averageBox.innerHTML = this.average;
+		this.scoreBox.innerHTML = this.score;
+
+		
+	}
 	
 } // end MemoryGame class 
