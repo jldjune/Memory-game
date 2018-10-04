bc5f53e8054ebb8490c6bf2a68966af9e394f79c
jldjune <jose.diaz23@gmail.com>
2018-10-04 10:51:43 -0400

    Added score saving and high scores display using AJAX and php.

--- js/memory-game.js
+++ js/memory-game.js
@@ -2,7 +2,7 @@
 
 class MemoryGame{
 	constructor(app, arr){
-		//
+		// initialize incoming app div
 		this.app = document.getElementById('app');
 		
 		// initialize incoming array
@@ -18,7 +18,7 @@ class MemoryGame{
 		// grab the footer that holds score and timer
 		this.footer = document.querySelector('footer');
 		
-		// grab the score box span 
+		// grab the score box spans 
 		this.attemptBox = document.getElementById('attempt-box');
 		this.matchBox = document.getElementById('match-box');
 		this.averageBox = document.getElementById('average-box');
@@ -30,10 +30,27 @@ class MemoryGame{
 		// grab timer span
 		this.timer = document.getElementById('timer');
 		
+		// grab high score box
+		this.highScoreBox = document.getElementById('high-score-box')
+		
 	} // end constructor
 	
 	// method runs when 'play' button is clicked
 	initGame() {
+		// ensure app div is clear
+		this.app.innerHTML = "";
+		
+		// endure high score box is hidden
+		this.highScoreBox.innerHTML = "";
+		this.highScoreBox.style.display = 'none';
+		this.highScoreBox.style.top = '1000px';
+		
+		// remove initials box and save score button, if they exist
+		if(this.initials){
+			this.footer.removeChild(this.initials);
+			this.footer.removeChild(this.saveBtn);
+		}
+		
 		// disbale play button
 		this.playBtn.disabled = true;
 		
@@ -47,11 +64,19 @@ class MemoryGame{
 		this.arr.sort((a, b) => 0.5 - Math.random());
 		
 		// get array size from select menu
-		this.gameSize = Number(document.getElementById('difficulty').value);
+		this.difficulty = document.getElementById('difficulty');
+		this.gameSize = Number(this.difficulty.value);
+		
+		// grab level text of option picked by user
+		this.level = this.difficulty.options[this.difficulty.selectedIndex].text;
+		// seperate selected text into array 
+		let levelArr = this.level.split(" ");
+		// set level to first part of text
+		this.level = levelArr[0];
 		
 		// score multiplier based on game difficulty
 		this.scale = this.gameSize ** 2;
-		alert(this.scale)
+		
 		// make a new array with a slice of the needed size
 		let gameArr = this.arr.slice(0, this.gameSize);
 //		
@@ -68,7 +93,6 @@ class MemoryGame{
 
 		// loop to make clickable images with class, name and ID properties
 		for(let i=0; i < gameArr.length; i++) {
-
 			let cardPic = new Image();
 			cardPic.src = 'images/final/200x200/' + gameArr[i] + '.jpg';
 			cardPic.className = 'cards';
@@ -76,13 +100,13 @@ class MemoryGame{
 			cardPic.name = gameArr[i];
 			app.appendChild(cardPic);
 			// click calls showPic method
-			cardPic.addEventListener('click', this.showPic.bind(this));
-			
+			cardPic.addEventListener('click', this.showPic.bind(this));	
 		}// end image for loop
 		
+		// message with 5 sec countdown before hiding cards
 		this.msgBox.innerHTML = 'HIDING ALL IMAGES IN <span id="countDown">5</span>';
 		
-		// 
+		// 5 sec countdown timer
 		let countdownInterval = setInterval(function(){
 			// update the countdown span tag number
 			let countdown = document.getElementById('countDown');
@@ -93,15 +117,17 @@ class MemoryGame{
 			// when countdown reaches zero, stop it
 			if(countdownVal == 0){
 				clearInterval(countdownInterval);
-			}
-		}, 1000)
+			}// end if clearInterval
+		}, 1000)// end countdownInterval
+		
+		// calls method to hide images
 		this.hidePic();
 		
 	}// end initGame
 	
 	// method displays 'hidden' images by changing src, tracks and displays score. runs when card image is clicked
 	showPic(){
-		// changes src of image clicked
+		// changes src of image clicked to display image
 		event.target.src = 'images/final/200x200/' + event.target.name + '.jpg';
 		
 		// array to hold images clicked
@@ -128,15 +154,15 @@ class MemoryGame{
 					}, 500)
 					
 				} else {// ID's don't match, it's a legit match
-			   		
+			   		// player matches a pair
 					this.msgBox.innerHTML = "Congrats! That's a Match!";
 					
-					// deactivate click event
+					// deactivate click event first image
 					let newPic0 = new Image();
 					newPic0.src = this.choicesArr[0].src;
 					newPic0.className = 'cards';
 					this.app.replaceChild(newPic0, this.choicesArr[0])
-					
+					// deactivate click event first image
 					let newPic1 = new Image();
 					newPic1.src = this.choicesArr[1].src;
 					newPic1.className = 'cards';
@@ -145,11 +171,10 @@ class MemoryGame{
 					// counts when player finds a match
 					this.matches++;
 					
-					// end game
-					this.match = this.gameSize
+					// end game when matches equal number of pairs
 					if(this.matches == this.gameSize){
 						// enable play button
-						this.playBtn.disbled = false;
+						this.playBtn.disabled = false;
 						
 						// output game over message
 						this.msgBox.innerHTML = "GAME OVER!";
@@ -157,13 +182,21 @@ class MemoryGame{
 						// stop timer by clearing timer interval
 						clearInterval(this.timerInterval);
 						
+						//input initials box
+						this.initials = document.createElement('input');
+						this.initials.type = 'text';
+						this.initials.id = 'initials';
+						this.initials.placeholder = 'AAA';
+						this.initials.setAttribute('maxlength', 3);
+						this.footer.appendChild(this.initials);
+						
+						
 						// make and output a clickable Save score button
 						let saveBtn = document.createElement('button');
 						saveBtn.innerHTML = 'SAVE SCORE';
 						saveBtn.addEventListener('click', this.saveScore.bind(this));
 						this.footer.appendChild(saveBtn);
 						
-						// click saveScore()
 					}
 					
 				}// end if ID comparison
@@ -172,7 +205,7 @@ class MemoryGame{
 				// output message
 				this.msgBox.innerHTML = 'Oops! Try Again!';
 				
-				//hide mismatched images at set delay
+				//hide mismatched images at set delay time
 				setTimeout(() => {
 					this.choicesArr[0].src = 'images/blank.png';
 					
@@ -226,8 +259,9 @@ class MemoryGame{
 				}
 				
 			*/
+			// starts timer 
 			this.startTimer();
-		}, 6500)
+		}, 6500)// end setTimeout
 	}// end of hidePic()
 	
 	// method creates a timer, is called by hidePic()
@@ -240,6 +274,7 @@ class MemoryGame{
 			this.totSec++
 			seconds++
 			
+			// increment minutes when seconds hit 60
 			if(seconds == 60){
 				seconds = 0
 				minutes++;	
@@ -255,15 +290,54 @@ class MemoryGame{
 				m = '0' + minutes;
 			}
 			
+			// timer output
 			this.timer.innerHTML = m + ':' + s;
 			
-		}, 1000)
+		}, 1000) // end timerInterval
 
 	}// end of startTimer()
 	
 	// save score method
 	saveScore(){
-		alert('SAVE SCORE')
-	}
-	
+		//alert('SAVE SCORE')
+		// display hidden high score box
+		this.highScoreBox.style.display = 'block';
+		this.highScoreBox.style.top = '150px';
+		this.highScoreBox.style.left = '350px';
+		
+		// create url with needed vars and values
+//		let url = 'php/memory-game.php?';
+//		url += 'score=' + this.score;
+//		url += '&attempts=' + this.attempts;
+//		url += '&matches=' + this.matches;
+//		url += '&seconds=' + this.totSec;
+//		url += '&level=' + this.level;
+//		url += '&initials=' + this.initials.value;
+//		
+//		let xhr = new XMLHttpRequest();
+//		xhr.onload = () => {
+//			this.highScoreBox.innerHTML = xhr.responseText;
+//		}
+//		
+//		xhr.open('GET', url, true);
+//		xhr.send()
+		
+		// jQuery AJAX version of the above
+		$.ajax({
+			method: 'get',
+			url: 'php/memory-game.php',
+			data: {
+				score: this.score,
+				attempts: this.attempts,
+				matches: this.matches,
+				seconds: this.totSec,
+				level: this.level,
+				initials: this.initials.value	
+			},
+			success: function(highScores){
+				$('#high-score-box').html(highScores)
+			}
+		})
+	}// end saveScore
+		
 } // end MemoryGame class 
---
+++ php/memory-game.php
@@ -0,0 +1,54 @@
+<?php 
+
+	// get incoming vars
+	$score = $_GET['score']; 
+	$attempts = $_GET['attempts'];
+	$matches = $_GET['matches'];
+	$seconds = $_GET['seconds'];
+	$level = $_GET['level'];
+	$initials = $_GET['initials'];
+	
+
+
+	// 2.) connect to MySQL
+	$conn = mysqli_connect('localhost', 'root', 'mysql') or die("Couldn't connect to MySQL");
+
+	// 3.) connect to database
+	mysqli_select_db($conn, 'memoryGame') or die("Couldn't find the Database");
+	
+	// query to enter new score
+$query = "INSERT INTO scores(score, attempts, matches, seconds, level, initials) 
+			VALUES('$score', '$attempts', '$matches', '$seconds', '$level', '$initials')";
+
+	mysqli_query($conn, $query);
+
+	// query to retrieve scores
+	$query2 = "SELECT * FROM scores 
+					WHERE matches < 10
+					ORDER BY score DESC
+					LIMIT 10";
+
+	$result  = mysqli_query($conn, $query2);
+	
+	// 
+	$tbl = '<h1> High Scores </h1>';
+	$tbl .= '<table cellpadding="6">';
+	$tbl .= '<tr><th>Rank</th><th>Player</th><th>Score</th>';
+	$tbl .= '<th>Level</th><th>Time(s)</th><th>Date</th></tr>';
+	
+	$i = 1;
+
+	while($row=mysqli_fetch_array($result)){
+		
+		$tbl .= "<tr><td>" . $i++ . "</td>"; 
+		$tbl .= "<td>" . strtoupper($row['initials']) . "</td>";
+		$tbl .= "<td>" . $row['score'] . "</td>";
+		$tbl .= "<td>" . $row['level'] . "</td>";
+		$tbl .= "<td>" . $row['seconds'] . "</td>";
+		$tbl .= "<td>" . date('D. M. d, Y - H:i:s', strtotime($row['dateTime'])) . "</td></tr>";
+	}
+
+	echo $tbl;
+
+
+?>
